e3edd780ab563b431ac1fded881f7f82eb33b578
Sangeeth Santhosh <73825180+sangeeths29@users.noreply.github.com>
2025-06-28 18:50:58 -0700

    Add files via upload

---
+++ 02-PyplotInMatplotlib.py
@@ -0,0 +1,21 @@
+# Problem 2 - Pyplot in Matplotlib ( https://www.geeksforgeeks.org/python-introduction-matplotlib/ )
+x = [0, 1, 2, 3, 4]
+y = [0, 1, 4, 9, 16]
+
+plt.plot(x, y)
+plt.show()
+
+import matplotlib.pyplot as plt
+
+x = [0, 2, 4, 6, 8]
+y = [0, 4, 16, 36, 64]
+
+fig, ax = plt.subplots()  
+ax.plot(x, y, marker='o', label="Data Points")
+
+ax.set_title("Basic Components of Matplotlib Figure")
+ax.set_xlabel("X-Axis") 
+ax.set_ylabel("Y-Axis")  
+
+
+plt.show()
